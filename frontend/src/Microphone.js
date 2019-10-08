b666c1a2f0f3ce38071cbf8f4af01780c9186fa6
xDroni <mrdroonix@gmail.com>
2019-10-08 18:50:14 +0200

    frontend init

---
+++ frontend/src/Microphone.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const micIsOnImage = require('./icons/mic-on-24px.svg');
+const micIsOffImage = require('./icons/mic-off-24px.svg');
+
+export default class Microphone extends React.Component {
+    state = {
+        micIsOn: true,
+        currentImage: micIsOnImage,
+    };
+
+    clickHandler = () => {
+        if(this.state.micIsOn) {
+            this.setState(prevState => ({
+                micIsOn: !prevState.micIsOn,
+                currentImage: micIsOffImage,
+            }));
+        } else {
+            this.setState(prevState => ({
+                micIsOn: !prevState.micIsOn,
+                currentImage: micIsOnImage,
+            }));
+        }
+
+    };
+
+    render() {
+        return (
+            <img src={this.state.currentImage} alt="microphone on" onClick={this.clickHandler}/>
+        )
+    }
+}
