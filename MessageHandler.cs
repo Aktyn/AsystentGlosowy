7ebe2c127d54c3b4e2c717759112facc62a9704b
Dawid <48656592+BlackSmithOzw@users.noreply.github.com>
2019-11-12 19:48:39 +0100

    adding a playlist and skipping to the next song in the playlist
    ithout saving yet

---
+++ MessageHandler.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Asystent.common;
+using Asystent.procedures;
+using Fleck;
+using Newtonsoft.Json;
+
+namespace Asystent {
+	internal enum MessageType {
+		SpeechResult = 1,
+		VideoFinished = 2
+	}
+
+	public class MessageHandler {
+		private static MessageHandler _handler;
+
+		private List<SpeechResult> _results;
+		private ulong _index;
+		private ProcedureBase _procedure;
+
+		private MessageHandler(List<SpeechResult> results, ulong index) {
+			_results = results;
+			_index = index;
+		}
+
+		private void Update(List<SpeechResult> updatedResults, ulong index) {
+			if( _index != index ) {
+				_index = index;
+				_procedure = null;//DISCARD PROCEDURE
+			}
+			_results = updatedResults;
+		}
+
+		private bool Execute(IWebSocketConnection clientConn) {
+			if( _procedure == null ) {
+				var matchingProcedures = ProcedureBase.MatchProcedures(_results);
+			
+				if (matchingProcedures.Count < 1)//no procedure matches result
+					return false;
+			
+				//Console.WriteLine(matchingProcedures.Count);
+			
+				if (matchingProcedures.Count > 1) {
+					Console.WriteLine("More than one procedure has been matched in single result");
+					//TODO: list those procedure names
+					return false;
+				}
+
+				_procedure = matchingProcedures.First();
+				_procedure.OnSendData += data => {
+					clientConn.Send(JsonConvert.SerializeObject(data));
+				};
+			}
+
+			_procedure.Update(_results);
+		
+			return _procedure.IsFinished();
+		}
+
+		private ProcedureBase GetProcedure() {
+			return _procedure;
+		}
+
+		private static SpeechResponse HandleSpeechResult(SpeechMessageSchema data, 
+			IWebSocketConnection clientConn) 
+		{
+			if( _handler != null )
+				_handler.Update(data.results, data.result_index);
+			else
+				_handler = new MessageHandler(data.results, data.result_index);
+
+			bool executed = _handler.Execute(clientConn);
+			ProcedureBase procedure = _handler.GetProcedure();
+
+			if (!executed) {
+				var resp = new SpeechResponse {res = "ignored"};
+				if(procedure != null)
+					resp.procedure = procedure.GetType().Name;
+				return resp;
+			}
+			
+			if (procedure == null)
+				_handler = null;
+			Console.WriteLine("Command executed, sending response to client");
+			return new SpeechResponse{res = "executed", index = data.result_index};
+		}
+		
+		public static void OnMessage(string message, IWebSocketConnection clientConn) {
+			Console.WriteLine("Message: " + message);
+			//example message:
+			//"{"type":1,"results":[{"result":"zagraj costam","confidence":0.618,"type":2}],"result_index":0}"
+
+			try {
+				object data = JsonConvert.DeserializeObject<MessageSchema>(message);
+				object response = null;
+
+				switch ((MessageType) ((MessageSchema)data).type) {
+					case MessageType.SpeechResult:
+						response = HandleSpeechResult(
+							JsonConvert.DeserializeObject<SpeechMessageSchema>(message),
+							clientConn
+						);
+						break;
+					case MessageType.VideoFinished:
+						Console.WriteLine("Finished video: " + JsonConvert.DeserializeObject<VideoFinishedMessageSchema>(message).video_id);
+						//TODO: update playlist state
+						clientConn.Send(JsonConvert.SerializeObject(procedures.ProcedureBase.playlistMemory[0]));
+						procedures.ProcedureBase.playlistMemory.RemoveAt(0);
+						break;
+					default:
+						Console.WriteLine("Incorrect message format");
+						break;
+				}
+
+				if (response != null)
+					clientConn.Send(JsonConvert.SerializeObject(response));
+			}
+			catch (JsonReaderException) {
+				Console.WriteLine("Cannot parse message as JSON");
+			}
+		}
+	}
+}
---
+++ ProcedureBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Asystent.common;
+
+namespace Asystent.procedures {
+	enum ResultType {
+		INTERIM = 1,
+		FINAL = 2,
+		ALTERNATIVE = 3
+	}
+	
+	public delegate void SendDataListener(object data);
+
+	public abstract class ProcedureBase {
+		private static Type[] procedures = { typeof(Example), typeof(YoutubePlay), typeof(YoutubeVolume), typeof(YoutubeMute),
+		 typeof(VolumeUnmute), typeof(YoutubeIncVolume), typeof(YoutubeDecVolume), typeof(YoutubeStop), typeof(YoutubeResume),
+		 typeof(YoutubePlaylist), typeof(YoutubeNastepny)};
+		//public static Regex regex = null;
+
+		protected List<SpeechResult> Results;
+		protected bool Finished = false;
+		
+		public event SendDataListener OnSendData;
+
+		protected ProcedureBase() { }
+
+		abstract public void Update(List<SpeechResult> results);
+
+		protected void SendData(object data) {
+			OnSendData?.Invoke(data);
+		}
+
+		public bool IsFinished() {
+			return Finished;
+		}
+		
+		public static List<PlaylistSchema> playlistMemory = new List<PlaylistSchema>();
+
+		public static List<ProcedureBase> MatchProcedures(List<SpeechResult> results) {
+			List<ProcedureBase> matching = new List<ProcedureBase>();
+
+			foreach (Type procedureClass in procedures) {
+				FieldInfo regexField = procedureClass.GetField("regex",
+					BindingFlags.Public | BindingFlags.Static);
+
+				if (regexField != null) {
+					Regex procedureRegex = (Regex) regexField.GetValue(null);
+
+					//if at least one result matches regex - corresponding procedure is added to matching list
+					foreach (SpeechResult res in results) {
+						if (!procedureRegex.Match(res.result).Success) continue;
+						matching.Add( (ProcedureBase)Activator.CreateInstance(procedureClass) );
+						break;
+					}
+				}
+				else {
+					Console.WriteLine("Procedure must have static field \"regex\" of type Regex ({0})",
+						procedureClass.Name);
+				}
+			}
+
+			return matching;
+		}
+	}
+}
