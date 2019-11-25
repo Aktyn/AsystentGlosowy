642169954cbab6831c4f7f9199205af4707c35a7
xDroni <mrdroonix@gmail.com>
2019-11-25 19:43:40 +0100

    toast notification

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import {eventEmitter} from "../serverConnection";
 
-export default function Footer() {
-    return <div className='footer'>TODO</div>;
+export default class Footer extends React.Component {
+    //listener
+    notifyListener = this._onNotify.bind(this);
+
+    state = {
+      message: null,
+    };
+
+    componentDidMount() {
+        eventEmitter.on('notify', this.notifyListener);
+    }
+
+    componentWillUnmount() {
+        eventEmitter.off('notify', this.notifyListener);
+    }
+
+    _onNotify(message) {
+        this.setState({
+            message: message,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                message: null,
+            });
+        }, 3000);
+    }
+
+    render() {
+        return <div className='footer'>{this.state.message}</div>;
+    }
 }
